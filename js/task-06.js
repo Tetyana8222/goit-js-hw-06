f77922a926fc90d6b1cc9a467b4a8d62a243702e
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-02-21 19:00:57 +0300

    task-6 in process

---
+++ js/task-06.js
@@ -0,0 +1,8 @@
+const validationInput = document.querySelector("#validation-input");
+
+// validationInput.addEventListener("focus", () => {
+//   if (validationInput) textInput.value = "This input has focus";
+// });
+function multiply(x, y, z) {
+  console.log(`Результат множення дорівнює ${x * y * z}`);
+}
