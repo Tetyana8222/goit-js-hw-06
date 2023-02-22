7dfab936658d34803205400739fea70a200800a6
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-02-22 20:16:17 +0300

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", submitHundler);
+
+function submitHundler(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  // console.log(email.value, password.value);
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+
+  const userLoginDetails = { email: email.value, Password: password.value };
+
+  console.log(userLoginDetails);
+  event.currentTarget.reset();
+}
