8c960061a43ac6121a81e7af709314a83ab40f78
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-02-23 09:58:34 +0300

    task 10 in progress

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlsEl = document.querySelector("#controls");
+const inputEl = document.querySelector("[type=number]");
+const btnCreateEl = document.querySelector(".button[data-create]");
+const btnDestroyEl = document.querySelector(".button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+btnCreateEl.addEventListener("click", createBoxes);
+btnDestroyEl.addEventListener("click", destroyBoxes);
+
+const createBoxes = (amount) => {
+  let size = 30;
+  const divArray = [];
+  for (let i = 0; i <= amount; i += 1) {
+    size += 10;
+    return divArray.push(div);
+  }
+};
+
+const destroyBoxes = () => {
+  boxesEl.innerHTML = "";
+};
