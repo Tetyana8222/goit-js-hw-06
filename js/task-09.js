81d0164e5b21a746cd82add5075cbd8a58a35950
Tetyana Kolisnyk <kolesnik.tm@gmail.com>
2023-02-23 09:00:12 +0300

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onClickButtonHandler = () => {
+  let currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+  spanEl.textContent.color = currentColor;
+};
+buttonEl.addEventListener("click", onClickButtonHandler);
